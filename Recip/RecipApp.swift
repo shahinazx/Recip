d58be60ed4b5137a27aa475bfbac59353c664de9
shahinaz <shahinazalsubaie@gmail.com>
2024-10-21 15:20:25 +0300

    Initial Commit

---
+++ Recip/RecipApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipApp.swift
+//  Recip
+//
+//  Created by Shahinaz Alsubaie on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RecipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
